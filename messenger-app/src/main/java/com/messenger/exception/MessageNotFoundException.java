8b082fb3aab78cdc85789643c9525f057b4a104a
fabricio <fabricio.henrique7@hotmail.com>
2019-02-27 16:42:41 -0300

    Fix exception description

--- messenger-app/src/main/java/com/messenger/exception/MessageNotFoundException.java
+++ messenger-app/src/main/java/com/messenger/exception/MessageNotFoundException.java
@@ -3,6 +3,6 @@ package com.messenger.exception;
 public class MessageNotFoundException extends RuntimeException {
 
     public MessageNotFoundException(String id) {
-        super("Room not found for id: " + id);
+        super("Message not found for id: " + id);
     }
 }
